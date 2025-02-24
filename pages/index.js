94a6e822b433651401ee478312626925a604336b
Tiago Alcântara <126494106+TiagoAlcan@users.noreply.github.com>
2025-02-24 17:59:55 +0000

    adiciona descricao - ideia mcdonalds

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
 function Home() {
-  return <h1>Seja bem-vindo ao meu mundo!</h1>;
+  return (
+    <div>
+      <h1>Seja bem-vindo ao meu facilitador de vendas!</h1>
+      <Description />
+    </div>
+  );
+}
+
+function Description() {
+  return (
+    <h2>
+      As pessoas que eu gostaria de ajudar são aquelas que se assemelham a mim.
+      Quem nunca teve um item em casa que gostaria de vender, mas não o vende
+      por conta de tempo?
+    </h2>
+  );
 }
 
 export default Home;
